73d84016c9b28741fe17332d68117ea043d6b855
jinwook-song <wlsdnr129@naver.com>
2021-12-30 16:37:57 +0900

    run express server and integrate socket.io

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log('helloooo');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket);
+});
+
+server.listen(port, () => {
+  console.log(`✅ Listening on http://localhost:${port}`);
+});
